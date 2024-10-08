bec0b6c4bb226b80e857fd8df352a083fda02ab4
Daksha B <daksha.bhusnur18@gmail.com>
2024-10-08 22:35:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Search from "./components/Search";
+import Detail from "./components/Detail";
+import "./App.css";
+
+function App() {
+    const [state, setState] = useState({
+        s: "sherlock",
+        results: [],
+        selected: {},
+    });
+
+    const apiurl =
+        "https://www.omdbapi.com/?apikey=a2526df0";
+
+    const searchInput = (e) => {
+        let s = e.target.value;
+
+        setState((prevState) => {
+            return { ...prevState, s: s };
+        });
+    };
+
+    const search = (e) => {
+        if (e.key === "Enter") {
+            axios(apiurl + "&s=" + state.s).then(
+                ({ data }) => {
+                    let results = data.Search;
+
+                    console.log(results);
+
+                    setState((prevState) => {
+                        return {
+                            ...prevState,
+                            results: results,
+                        };
+                    });
+                }
+            );
+        }
+    };
+
+    const openDetail = (id) => {
+        axios(apiurl + "&i=" + id).then(({ data }) => {
+            let result = data;
+
+            setState((prevState) => {
+                return { ...prevState, selected: result };
+            });
+        });
+    };
+
+    const closeDetail = () => {
+        setState((prevState) => {
+            return { ...prevState, selected: {} };
+        });
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="header-content">
+                <img
+                    src={`${process.env.PUBLIC_URL}/camera.png`}
+                    alt="camera"
+                    className="header-image"
+                />
+                <h1>Binge-Buddy</h1>
+                </div>
+            </header>
+
+            <main>
+                <Search
+                    searchInput={searchInput}
+                    search={search}
+                />
+
+                <div className="results">
+                    {state.results.map((e) => (
+                    <div
+                        className="result"
+                        key={e.imdbID} // It's good practice to add a unique key
+                        onClick={() => openDetail(e.imdbID)}
+                    >
+                    <img
+                        style={{ width: "200px" }}
+                        src={e.Poster !== 'N/A' ? e.Poster : `${process.env.PUBLIC_URL}/placeholder-vertical.jpg`}
+                        alt={e.Title}
+                        className="result-image" // Optionally, add a class for additional styling
+                    />
+      <h3>{e.Title}</h3>
+    </div>
+  ))}
+</div>
+
+
+                {typeof state.selected.Title !=
+                "undefined" ? (
+                    <Detail
+                        selected={state.selected}
+                        closeDetail={closeDetail}
+                    />
+                ) : (
+                    false
+                )}
+            </main>
+        </div>
+    );
+}
+
+export default App;
